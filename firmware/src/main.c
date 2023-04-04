91e4cbf4b07baf5941e5e95483e81f782fd1511b
Brad McGarvey <45929861+bemcgarvey@users.noreply.github.com>
2023-04-03 20:02:37 -0600

    Tested pressure sensor

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -26,6 +26,7 @@
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"                // SYS function prototypes
+#include "ms5637.h"
 
 
 // *****************************************************************************
@@ -37,14 +38,18 @@
 int main ( void )
 {
     int count = 0;
+    int tick = 0;
     /* Initialize all modules */
     SYS_Initialize ( NULL );
-
+    printf("Starting\r\n");
+    ms5637Init();
     while ( true )
     {
         ++count;
         if (count == 1000000) {
             count = 0;
+            ++tick;
+            //printf("Tick = %d\r\n", tick);
             LED_Toggle();
         }
         /* Maintain state machines of all polled MPLAB Harmony modules. */
